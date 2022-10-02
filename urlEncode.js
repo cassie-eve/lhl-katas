f1fa6cf23920b6e4fb0c6c544d6d599fefa3c03b
Cassandra Keddis <cassandra@thinkific.com>
2022-10-02 11:55:21 -0700

    encoding string kata

---
+++ urlEncode.js
@@ -0,0 +1,15 @@
+const urlEncode = function(text) {
+  let newUrl = '';
+  for (let letter of text.trim()) {
+    if (letter === ' ') {
+      newUrl += '%20'
+    } else {
+      newUrl += letter;
+    }
+  }
+  return newUrl;
+};
+
+console.log(urlEncode("Lighthouse Labs"));
+console.log(urlEncode(" Lighthouse Labs "));
+console.log(urlEncode("blue is greener than purple for sure"));
