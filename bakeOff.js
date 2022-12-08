b7af0e246fd76ae094d4185869dd4739ccceaacd
Cassandra Keddis <cassandra@thinkific.com>
2022-12-07 18:23:20 -0800

    great codeville bakeoff function

---
+++ bakeOff.js
@@ -0,0 +1,52 @@
+const ingredientCheck = function(bakeryArr, recipe) {
+  return bakeryArr.some(value => recipe.includes(value));
+};
+
+const chooseRecipe = function(bakeryA, bakeryB, recipes) {
+  for (let recipe of recipes) {
+    if (ingredientCheck(bakeryA, recipe.ingredients)) {
+      if (ingredientCheck(bakeryB, recipe.ingredients)) {
+        return recipe.name;
+      }
+    }
+  }
+  return `No matching ingredients`;
+};
+
+let bakeryA = ['saffron', 'eggs', 'tomato paste', 'coconut', 'custard'];
+let bakeryB = ['milk', 'butter', 'cream cheese'];
+let recipes = [
+  {
+    name: 'Coconut Sponge Cake',
+    ingredients: ['coconut', 'cake base']
+  },
+  {
+    name: 'Persian Cheesecake',
+    ingredients: ['saffron', 'cream cheese']
+  },
+  {
+    name: 'Custard Surprise',
+    ingredients: ['custard', 'ground beef']
+  }
+];
+
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+
+bakeryA = ['potatoes', 'bay leaf', 'raisins'];
+bakeryB = ['red bean', 'dijon mustard', 'apples'];
+recipes = [
+  {
+    name: 'Potato Ganache',
+    ingredients: ['potatoes', 'chocolate']
+  },
+  {
+    name: 'Sweet Fish',
+    ingredients: ['anchovies', 'honey']
+  },
+  {
+    name: "Nima's Famous Dijon Raisins",
+    ingredients: ['dijon mustard', 'raisins']
+  }
+];
+
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
