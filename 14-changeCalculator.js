96eb583d8210d518355a4891d4a4c65fe359df95
Cassandra Keddis <cassandra@thinkific.com>
2022-12-13 16:52:16 -0800

    create placeholders for each remaining kata

---
+++ 14-changeCalculator.js
@@ -0,0 +1,11 @@
+const calculateChange = function(total, cash) {
+  let change = cash - total;
+  let count = 0;
+  const units = ['Twenty', 'Ten', 'Five', 'Toonie', 'Loonie', 'Quarter', 'Dime', 'Nickel', 'Penny'];
+
+  //while with if loop inside
+};
+
+console.log(calculateChange(1787, 2000));
+console.log(calculateChange(2623, 4000));
+console.log(calculateChange(501, 1000));
